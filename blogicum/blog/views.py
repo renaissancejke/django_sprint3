eb82f8476a379793d8bc5b6a88b239f3148c3765
Ростислав Краснощеков <rkrasnoshchekov@gmail.com>
2024-01-19 16:52:16 +0300

    first_commit

---
+++ blogicum/blog/views.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post, Category
+
+
+def posts():
+    return Post.objects.select_related(
+        'category',
+        'location',
+        'author'
+    ).filter(
+        is_published=True,
+        category__is_published=True,
+        pub_date__lte=datetime.now()
+    )
+
+
+def index(request):
+    return render(request, 'blog/index.html', {'post_list': posts()[:5]})
+
+
+def post_detail(request, id):
+    post = get_object_or_404(posts(), id=id)
+    return render(request, 'blog/detail.html', {'post': post})
+
+
+def category_posts(request, category_slug):
+    category = get_object_or_404(
+        Category,
+        slug=category_slug,
+        is_published=True
+    )
+    context = {'category': category,
+               'post_list': posts().filter(category=category)}
+    return render(request, 'blog/category.html', context)
